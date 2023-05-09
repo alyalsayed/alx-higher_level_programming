079c07d38fc048762c5326fcd3b253c129fa2687
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-09 18:54:24 -0400

    fix file 3-print-alphapet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-	ch=chr(i) 
-	if  ch != 'q' and ch != 'e':
-		print("{}".format(ch, end="")
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
