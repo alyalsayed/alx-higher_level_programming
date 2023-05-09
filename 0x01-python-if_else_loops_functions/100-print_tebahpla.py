da737d8e99bf3003c709e893235b395ef206d99b
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-09 19:27:35 -0400

    upload task15

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
-#!/usr/bin/ython3
+#!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{0}{1}".format(chr(i), chr(i - 32)), end="")
+    if i % 2 == 0:
+        n = chr(i)
+    else:
+        n = chr(i-32)
+    print("{}".format(n), end="")
