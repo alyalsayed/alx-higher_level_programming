f434c922bd99fca1fa4e1d5ee65ec510e0b60488
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-11 10:37:33 -0400

    done : finished task 2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
-def main():
-    args = sys.argv[1:]
-    total = sum(map(int, args))
-    print(total)
+if __name__ != "__main__":
+    exit()
 
-if __name__ == "__main__":
-    main()
+argc = len(sys.argv) - 1
+
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
