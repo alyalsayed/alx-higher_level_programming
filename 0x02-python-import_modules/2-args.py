c326ed6e0edd74c1e9cf1b9633350305eb69d36d
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-11 10:31:40 -0400

    done : finished task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
-if __name__ == "__main__":
+def main():
     args = sys.argv[1:]
     num_args = len(args)
 
     # Print number of arguments
-    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
+    print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
 
     # Print list of arguments
     for i, arg in enumerate(args):
         print("{}: {}".format(i+1, arg))
+
+if __name__ == "__main__":
+    main()
