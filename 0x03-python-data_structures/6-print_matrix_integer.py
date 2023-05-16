93875cb734dbbfbe4d1b6671f7be17fb074210e5
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-16 16:02:17 -0400

    done: upload task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row)-1:
+                print("{:d}".format(row[i]), end="")
+            else:
+                print("{:d}".format(row[i]), end=" ")
+        print("")
