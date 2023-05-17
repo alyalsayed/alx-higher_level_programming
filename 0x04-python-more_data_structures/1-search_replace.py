85b3b98cd51968c0492ce0924485f664a370424b
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-17 17:29:16 -0400

    done : upload task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    # Use a list comprehension to replace the elements
-    new_list = [replace if item == search else item for item in my_list]
-    
-    # Return the new list
-    return new_list
+    return [replace if item == search else item for item in my_list]
