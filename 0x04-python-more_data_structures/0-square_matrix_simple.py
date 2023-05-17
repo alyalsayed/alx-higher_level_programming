3ffae95da8458e0950f159b9f287f4d18e48f120
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-17 17:21:02 -0400

    done : upload task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Use a list comprehension to compute the square of each element in the matrix
-    result = [[item ** 2 for item in row] for row in matrix]
+     # Create an empty matrix of the same size as the input matrix
+    result = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    
+    # Iterate over each element in the input matrix and compute its square
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = matrix[i][j] ** 2
     
     # Return the new matrix
     return result
