3aac95a496327b8fec18ad6c169c422603e2d48f
alyalsayed <20812019100880@eng.zu.edu.eg>
2023-05-17 17:23:23 -0400

    done : upload task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            result[i][j] = matrix[i][j] ** 2
-    return result
-
+    return [[y ** 2 for y in x] for x in matrix]
