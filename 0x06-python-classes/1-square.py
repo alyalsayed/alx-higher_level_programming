f742133a8619055456576d52b9e2cce93a810f5a
alyalsayed <alyalsayed56@yahoo.com>
2023-05-31 02:17:33 +0300

    upload task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Define an empty class Square."""
 
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except IndexError:
-            break
-    print("")
-    return count
+
+class Square:
+    """Square class
+    """
+    def __init__(self, size):
+        """Initialize a new instance of Square
+
+        Args:
+        size: The size of the new Square object
+        """
+        self.__size = size
