3594b9845d106527ba1623891b998a30d8402f93
alyalsayed <alyalsayed56@yahoo.com>
2023-05-31 02:20:13 +0300

    delete non-necessary files

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,84 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except IndexError:
-            break
-    print("")
-    return count
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
+    def __init__(self, size=0):
+        """
+        initialization function for our square clasee
+        """
+        if self.__validate_size(size):
+            self.__size = size
+
+    def __eq__(self, other):
+        """
+        used by == to check equality
+        """
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        """
+        used by != to check equality
+        """
+        return (self.area() != other.area())
+
+    def __lt__(self, other):
+        """
+        used by < to check equality
+        """
+        return (self.area() < other.area())
+
+    def __le__(self, other):
+        """
+        used by <= to check equality
+        """
+        return (self.area() <= other.area())
+
+    def __gt__(self, other):
+        """
+        used by > to check equality
+        """
+        return (self.area() > other.area())
+
+    def __ge__(self, other):
+        """
+        used by >= to check equality
+        """
+        return (self.area() >= other.area())
+
+    @property
+    def size(self):
+        """
+        getter for size attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter for size attribute
+        """
+        if self.__validate_size(value):
+            self.__size = value
+
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
+
+    def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return True
+        return False
