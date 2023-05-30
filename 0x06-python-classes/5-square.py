7b86dd21f554dcd9751be652277dbbdbc7206e42
alyalsayed <alyalsayed56@yahoo.com>
2023-05-31 02:18:44 +0300

    upload task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python3
+"""Define an empty class Square."""
 
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except IndexError:
-            break
-    print("")
-    return count
+
+class Square:
+    """Square class
+    """
+    def __init__(self, size=0):
+        """Initialize a new instance of Square
+
+        Args:
+        size: The size of the new Square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self) -> int:
+        """Get a Square instance area
+        Return: int
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self) -> int:
+        """Get a Square instance size
+        Return: int"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set a Square instance size
+        Args:
+        value: The new size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if (self.__size == 0):
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
