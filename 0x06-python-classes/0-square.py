8ef97ca9cd7bff8759fecbd4d0c505f238017ed1
alyalsayed <alyalsayed56@yahoo.com>
2023-05-30 19:28:32 +0300

    finish uploading task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except IndexError:
-            break
-    print("")
-    return count
+class Square:
+    pass
