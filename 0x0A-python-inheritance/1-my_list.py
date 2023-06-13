de4ba1a7a0b509ca41bb6c8c826d0d500b67ad4a
alyalsayed <alyalsayed56@yahoo.com>
2023-06-13 04:05:45 +0300

    finish uploading task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""MyList module"""
+
+
+class MyList(list):
+    """MyList
+    """
+
+    def print_sorted(self):
+        """print_sorted
+        """
+        print(sorted(self))
