6468244524abcc22f19489245874a410c21c6a04
alyalsayed <alyalsayed56@yahoo.com>
2023-06-14 07:43:47 +0300

    feat : uploading task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Student to json module"""
+
+
+class Student:
+    """Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """__init__: Initialize a new student
+
+        Args:
+                first_name (str): Student firstname
+                last_name (str): Student lastname
+                age (int): Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json
+
+             Args:
+                     attrs (any, None): Attributes. Defaults to None.
+             Returns:
+                     dict: Dictionary representation of the instance
+             """
+        if (type(attrs) == list
+                and all(type(attr) == str for attr in attrs)):
+            return {
+                k: self.__dict__[k]
+                for k in attrs
+                if attrs and self.__dict__.get(k)
+            }
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload_from_json
+
+        Args:
+            json (dict): The key/value replacement.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
