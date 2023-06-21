f113a71599d04cfcc03a7b919e0e5d688898a77b
alyalsayed <alyalsayed@kali.aly.com>
2023-06-21 14:28:29 +0200

    commit message

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,13 +3,18 @@
 '''
 
 class Base:
-	'''A representation of the base of our OOP hierarchy.
+	'''A representation of the base class.
 	'''
 	__nb_objects = 0
 
 	def __init__(self, id=None):
-	if id is not None:
-	    self.id = id
-	else:
-	    Base.__nb_objects += 1
-	    self.id = Base.__nb_objects
+		"""Initialize new Base instance
+
+		    Args:
+		            id (int, None): Instance id. Defaults to None.
+		    """
+		if id is not None:
+		    self.id = id
+		else:
+		    Base.__nb_objects += 1
+		    self.id = Base.__nb_objects
