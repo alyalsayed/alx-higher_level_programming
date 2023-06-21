f3afd70cccbbb5a5daa74955d5ac0b5f63941597
alyalsayed <alyalsayed@kali.aly.com>
2023-06-21 14:55:41 +0200

    feat : upload task 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,8 +2,10 @@
 '''Module for Rectangle class.'''
 from models.base import Base
 
+
 class Rectangle(Base):
 	'''A Rectangle class.'''
+
 	def __init__(self, width, height, x=0, y=0, id=None):
 		'''Constructor.'''
 		super().__init__(id)
@@ -14,32 +16,49 @@ class Rectangle(Base):
 
 	@property
 	def width(self):
+		'''Width of this rectangle.'''
 		return self.__width
 
 	@width.setter
 	def width(self, value):
+		self.is_integer("width", value, False)
 		self.__width = value
 
 	@property
 	def height(self):
+		'''Height of this rectangle.'''
 		return self.__height
 
 	@height.setter
 	def height(self, value):
+		self.is_integer("height", value, False)
 		self.__height = value
 
 	@property
 	def x(self):
+		'''x of this rectangle.'''
 		return self.__x
 
 	@x.setter
 	def x(self, value):
+		self.is_integer("x", value)
 		self.__x = value
 
 	@property
 	def y(self):
+		'''y of this rectangle.'''
 		return self.__y
 
 	@y.setter
 	def y(self, value):
-        	self.__y = value
+		self.is_integer("y", value)
+		self.__y = value
+        	
+        def is_integer(self, name, value, eq=True):
+		'''Method for validating the value.'''
+		if type(value) != int:
+		    raise TypeError("{} must be an integer".format(name))
+		if eq and value < 0:
+		    raise ValueError("{} must be >= 0".format(name))
+		elif not eq and value <= 0:
+		    raise ValueError("{} must be > 0".format(name))
