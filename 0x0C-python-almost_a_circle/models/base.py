338aa4a1ff331f64774746032bca9f9e22c94b61
alyalsayed <alyalsayed@kali.aly.com>
2023-06-21 14:31:21 +0200

    commit message

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-'''Module for Base class.
-'''
+'''Module for Base class.'''
+
 
 class Base:
-	'''A representation of the base class.
-	'''
-	__nb_objects = 0
+    '''A representation of the base of our OOP hierarchy.'''
 
-	def __init__(self, id=None):
-		"""Initialize new Base instance
+    __nb_objects = 0
 
-		    Args:
-		            id (int, None): Instance id. Defaults to None.
-		    """
-		if id is not None:
-		    self.id = id
-		else:
-		    Base.__nb_objects += 1
-		    self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        '''Constructor.'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
