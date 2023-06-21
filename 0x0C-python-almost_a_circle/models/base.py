de648c133e7167255d1d399ee87920e30564d2ae
alyalsayed <alyalsayed@kali.aly.com>
2023-06-21 14:22:58 +0200

    -m

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-class Base:
-    __nb_objects = 0
+'''Module for Base class.
+'''
 
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+class Base:
+	'''A representation of the base of our OOP hierarchy.
+	'''
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+	if id is not None:
+	    self.id = id
+	else:
+	    Base.__nb_objects += 1
+	    self.id = Base.__nb_objects
