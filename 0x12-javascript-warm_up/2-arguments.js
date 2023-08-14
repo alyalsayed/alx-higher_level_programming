292e68c7ff500994a4ac75283ee3bbf746d3e9be
alyalsayed <alyalsayed@kali.aly.com>
2023-08-14 10:33:10 +0200

    alx: done task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* A script to print a message depending
+   on the number of arguments .
+*/
+const numArgv = process.argv.length;
+if (numArgv < 3) {
+  console.log('No argument');
+} else if (numArgv === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
