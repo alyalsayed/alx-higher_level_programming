7bd0228586b1be58b99538b567f3fd215473d72a
alyalsayed <alyalsayed@kali.aly.com>
2023-08-14 10:40:05 +0200

    alx: done task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+A script that to print 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop 
+*/
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let j = 0; j < myArray.length; j++) {
+  console.log(myArray[j]);
+}
