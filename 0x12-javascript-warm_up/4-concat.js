fd4ca618ff5869869aabb758a37c596c506f300a
alyalsayed <alyalsayed@kali.aly.com>
2023-08-14 10:37:45 +0200

    alx: edit some lines

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-/* A script to print two arguments passed to it,
-   in the following format: “ is ” 
+/* 
+A script to print two arguments passed to it,
+in the following format: “ is ” 
 */
 
 console.log(process.argv[2] + ' is ' + process.argv[3]);
