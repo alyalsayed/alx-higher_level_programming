6f7474fb9641dd684ebbdf34313fc1dbb6e92c9d
alyalsayed <alyalsayed56@yahoo.com>
2023-09-05 01:06:06 +0300

    upload new task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
