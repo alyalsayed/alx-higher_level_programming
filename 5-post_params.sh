f5fed208fd0aed42788ae6f1efb699ac1c2ca453
alyalsayed <alyalsayed56@yahoo.com>
2023-09-05 00:59:41 +0300

    new task

--- 5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
